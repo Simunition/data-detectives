56205eec994f149752c247034eb6b7c09afef0c8
Arthur Neumann <arthur.neumann8@gmail.com>
2023-06-07 15:00:30 -0700

    Added comments to code

--- GRIBLoader.py
+++ GRIBLoader.py
@@ -1,6 +1,12 @@
 import ProcesssGRIB as pg
 
 grib_input = 'GRIBFiles/US058GMET-GR1mdl.0018_0056_00000F0RL2023012512_0006_000000-000000geop_ht'
+
+#Convert GRIB to XArray
 x_out = pg.input_conversion(grib_input)
-pg.csv_conversion(x_out)
-print(x_out)
+
+#Testing and CSV Output
+if __name__ == "__main__":
+    #Convert XArray to CSV
+    pg.csv_conversion(x_out)
+    print(x_out)
--- ProcesssGRIB.py
+++ ProcesssGRIB.py
@@ -1,12 +1,22 @@
 import xarray as xr
 import cfgrib
 
+#Function to convert GRIB input to XArray
 def input_conversion(grib_file):
+    
+    #Process GRIB file using cfgrib
     with xr.open_dataset(grib_file, engine='cfgrib') as mf:
         return mf.to_array()
 
+#Function to convert XArray input to CSV file
 def csv_conversion(meteo_arr):
     meteo_arr.name = 'met_d'
+
+    #Convert to Dataset
     met_data = meteo_arr.to_dataset()
+    
+    #Convert to Dataframe
     met_frame = met_data.to_dataframe()
+    
+    #Convert to CSV
     met_frame.to_csv('Output/csv_out.csv')
