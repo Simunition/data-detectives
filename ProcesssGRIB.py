d3dd9814a37fa30d07ebfae34e35a76b2ab3cfd2
Arthur Neumann <arthur.neumann8@gmail.com>
2023-06-07 14:52:35 -0700

    Updated Output

--- ProcesssGRIB.py
+++ ProcesssGRIB.py
@@ -9,4 +9,4 @@ def csv_conversion(meteo_arr):
     meteo_arr.name = 'met_d'
     met_data = meteo_arr.to_dataset()
     met_frame = met_data.to_dataframe()
-    met_frame.to_csv('csv_out.csv')
+    met_frame.to_csv('Output/csv_out.csv')
