be65b58c593ad7a5e0cfeed7662e2d9b90b5b7f0
Arthur Neumann <arthur.neumann8@gmail.com>
2023-06-12 11:08:39 -0700

    added testing

--- GRIBPlotter.py
+++ GRIBPlotter.py
@@ -1,13 +1,13 @@
+import GRIBLoader as gl
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 
-# Function to load and plot XArray data
-def plot_grib(xarray_input):
-    # Load the XArrary and extract the data
-    ds = xarray_input
-    data = ds.data_vars['gh'].values
-    lats = ds.coords['latitude'].values
-    lons = ds.coords['longitude'].values
+# Function to plot XArray data on a world map
+def plot_grib(xarray_data):
+    # Extract the data and coordinates
+    data = xarray_data['gh'].values
+    lats = xarray_data['latitude'].values
+    lons = xarray_data['longitude'].values
 
     # Create a plot with a world map using Cartopy
     fig = plt.figure(figsize=(10, 6))
@@ -26,3 +26,8 @@ def plot_grib(xarray_input):
 
     # Show the plot
     plt.show()
+
+if __name__ == "__main__":
+    # Assuming you already have an XArray dataset, xarray_data
+    xarray_data = gl.convert_grib('/home/cfc/Python/GRIBFiles/US058GMET-GR1mdl.0018_0056_00000F0RL2023012512_0006_000000-000000geop_ht')
+    plot_grib(xarray_data)
