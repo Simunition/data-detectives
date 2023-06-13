997f6e5252e934f02c130fcbe55e3fc66346a036
Arthur Neumann <arthur.neumann8@gmail.com>
2023-06-13 12:11:51 -0700

    plot adjustments, coloring/legend

--- GRIBPlotter.py
+++ GRIBPlotter.py
@@ -1,5 +1,6 @@
 import GRIBLoader as gl
 import os
+import numpy as np
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 
@@ -23,14 +24,23 @@ def plot_grib(xarray_data):
     ax.stock_img()
     ax.gridlines(draw_labels=True)
 
+    # Compute plot min/max and set the contour value range
+    vmin = data.min().item()
+    vmax = data.max().item()
+    levels = np.linspace(vmin, vmax, 100)
+
     # Plot the data
-    plt.contourf(lons, lats, data, transform=ccrs.PlateCarree())
+    plt.contourf(lons, lats, data, levels=levels, transform=ccrs.PlateCarree())
+
+    # Add colorbar
+    cbar = plt.colorbar(shrink=0.642,  pad=0.1)
+    cbar.set_label('Geopotential Height (meters)')
 
     # Set title and labels
     plt.title('500hPa Geopotential Height Zero-Hour Forecast for ' + month + '/' + day + '/' + year)
     plt.xlabel('Longitude')
     plt.ylabel('Latitude')
-    
+
     # Save plot
     output_filename = "zero_hour_plot.png"
     output_path = os.path.join('Output/', output_filename)
