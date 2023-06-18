b4ca9a12a2c25807ee107496d8fa6f6a4592d960
tyler-wilkie <135461551+tyler-wilkie@users.noreply.github.com>
2023-06-18 12:27:14 -0400

    Finalized API
    Finalized API, two files are saved in static folder, the png and the csv. Both can be called for display and output from the webapp.

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,15 @@
 # __init__.py
 
 import os
-from flask import Flask, render_template, Response, request, redirect, url_for
+from flask import Flask, render_template, Response, request, redirect, url_for, make_response
 from werkzeug.utils import secure_filename
 import requests
 
 app = Flask(__name__)
 
 app.config["UPLOAD_FOLDER"] = "static/"
+
+MODEL_URL = "http://127.0.0.1:5001"
 
 headings = ("Latitude","Longitude","Variable","Time","Step","Max Wind","Valid Time","met_d")
 data = (
@@ -30,11 +32,30 @@ def file_upload():
         #f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         #file = open(app.config['UPLOAD_FOLDER'] + filename, "r")
         #gribContent = file.read()
-        url = "http://127.0.0.1:5001/api" #heroku URL needed here
+        url = MODEL_URL + "/api" #heroku URL needed here
         forecast = requests.post(url, files={'gribFile': open(f_location,'rb')})
+
     return redirect(request.referrer)
 
+@app.route("/submitCsvOut", methods=['POST'])
+def submit_csv_out():
+    # get file from request.files
+    f = request.files['csvOut']
+    filename = secure_filename(f.filename)
+    f.save(app.config['UPLOAD_FOLDER'] + filename)
+    # save the file to the location
+    # return a 200 response with make_response
+    return "OK"
 
+@app.route("/submitPngOut", methods=['POST'])
+def submit_png_out():
+    # get file from request.files
+    f = request.files['pngOut']
+    filename = secure_filename(f.filename)
+    f.save(app.config['UPLOAD_FOLDER'] + filename)
+    # save the file to the location
+    # return a 200 response with make_response
+    return "OK"
 
 @app.route("/getCSV")
 def getCSV():
